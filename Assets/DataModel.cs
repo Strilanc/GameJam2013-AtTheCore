70fde0a14db858cf03ff77707da982e6c728d1bf
paul pettipas <paul.pettipas@gmail.com>
2013-10-19 14:50:12 -0300

    changes

---
+++ Assets/DataModel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DataModel : MonoBehaviour {
+	
+	public GameObject asteroid;
+	public GameObject shipt;
+	
+	public Dictionary<long,GameObject> allThings = new Dictionary<long, GameObject>();
+	
+	public void UpdateObject(long id){
+		GameObject go = allThings[id];
+		Ship ship  = go.GetComponent<Ship>();
+		Asteroid asteroid  = go.GetComponent<Asteroid>();
+		
+	}
+	
+	
+	
+	public void CreateWorld(int id){
+		GameObject obj = Instantiate(shipt);
+		
+	}
+	
+	
+	
+	public void CreateAsteroid(){
+		
+	}
+}
