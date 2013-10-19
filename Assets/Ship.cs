8bd79ee48cd4203ff6e114b2168b73f7bbe930ef
Craig Gidney <craig.gidney@gmail.com>
2013-10-19 14:48:23 -0300

    Engine thrust

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -1,54 +1,119 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
+using Random = UnityEngine.Random;
 
 public class Ship : MonoBehaviour {
     public GameObject SpeedStreak;
+    public ParticleSystem EngineLeftDownward;
+    public ParticleSystem EngineLeftUpward;
+    public ParticleSystem EngineLeftLeftward;
+    public ParticleSystem EngineLeftForeward;
+    public ParticleSystem EngineLeftBackward;
+
+    public ParticleSystem EngineRightDownward;
+    public ParticleSystem EngineRightRightward;
+    public ParticleSystem EngineRightUpward;
+    public ParticleSystem EngineRightForeward;
+    public ParticleSystem EngineRightBackward;
+
+    public ParticleSystem EngineBottomForeward;
+    public ParticleSystem EngineBottomBackward;
+
     private readonly List<GameObject> _speedStreak = new List<GameObject>(); 
 	void Start () {
     }
-	
-	void FixedUpdate () {
-        const float rotFactor = 2f;
-        const float sideThrustFactor = 50f;
-        var yaw = Input.GetAxis("Horizontal") * Time.deltaTime * rotFactor;
-        var pitch = Input.GetAxis("Vertical") * Time.deltaTime * rotFactor;
-        var roll = Input.GetAxis("Roll") * Time.deltaTime * rotFactor;
 
-        var sideToSide = Input.GetAxis("LeftRightThrust") * Time.deltaTime * sideThrustFactor;
-        var upDOwn = Input.GetAxis("UpDownThrust") * Time.deltaTime * sideThrustFactor;
+    void FixedUpdate() {
+        var yawInput = Input.GetAxis("Horizontal");
+        var pitchInput = Input.GetAxis("Vertical");
+        var rollInput = Input.GetAxis("Roll");
+        
+        var angularThrust = default(Vector3);
+        angularThrust += transform.up * yawInput;
+        angularThrust += transform.right * pitchInput;
+        angularThrust += transform.forward * rollInput;
 
-	    var rigidBody = GetComponent<Rigidbody>();
-        rigidBody.angularVelocity += transform.up * yaw;
-        rigidBody.angularVelocity += transform.right * pitch;
-        rigidBody.angularVelocity += transform.forward * roll;
+        var inputLeftRightThrust = Input.GetAxis("LeftRightThrust");
+        var inputUpDownThrust = Input.GetAxis("UpDownThrust");
+        var inputForwardBackThrust = Input.GetKey(KeyCode.Joystick1Button0) ? 1 : 0f;
+        var thrustVector = default(Vector3);
+        thrustVector += transform.forward.normalized * inputForwardBackThrust;
+        thrustVector += inputLeftRightThrust * -transform.right;
+        thrustVector += inputUpDownThrust * transform.up;
 
-        rigidBody.velocity += sideToSide * -transform.right;
-        rigidBody.velocity += upDOwn * transform.up;
+        var rigidBody = GetComponent<Rigidbody>();
 
-        var thrust = Input.GetKey(KeyCode.Joystick1Button0) ? 1 : 0;
-        rigidBody.velocity += transform.forward.normalized * 1f * thrust;
-        if (thrust != 0) {
+        if (inputForwardBackThrust != 0) {
             // rattle ship
-            var v = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1))/500;
+            var v = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1)) / 500;
             transform.rotation *= Quaternion.FromToRotation(Vector3.forward, Vector3.forward + v);
         }
 
-	    MakeSpeedStreak();
 
-	    var useBrakes = Input.GetKey(KeyCode.Joystick1Button1);
-	    var brakesPlus = useBrakes ? 100f : 0;
+        MakeSpeedStreak();
 
-	    // rotation compensation
-	    var rotationSpeed = rigidBody.angularVelocity.magnitude;
-        var autoBrakingSpeed = Mathf.Clamp((rotationSpeed - 0.1f) / 3 * Time.deltaTime, 0, rotationSpeed);
-	    rigidBody.angularVelocity -= rigidBody.angularVelocity.normalized*autoBrakingSpeed;
+        var useBrakes = Input.GetKey(KeyCode.Joystick1Button1);
+        var brakesPlus = useBrakes ? 100f : 0;
+        const float enginePower = 50f;
+        const float rotationalPower = 2f;
+        var brakingFactor = useBrakes ? 1f : 0.5f;
+        var rotationalBrakingFactor = useBrakes ? 1f : 0.5f;
 
-	    var glide = rigidBody.velocity - RestAtPoint(transform.position);
-        var autoBrakingSpeed2 = Mathf.Clamp((glide.magnitude - 0.1f) / 3 * Time.deltaTime, 0, glide.magnitude);
-        rigidBody.velocity -= glide.normalized * autoBrakingSpeed2;
-	}
+        // rotation compensation
+        if (angularThrust.magnitude < 0.1) {
+            var glide = rigidBody.angularVelocity;
+            var brakeThrustVector = -glide.normalized * rotationalBrakingFactor;
+            var velocityAfterBraking = rigidBody.angularVelocity + brakeThrustVector * rotationalPower * Time.deltaTime;
+            var overBraked = Mathf.Sign(Vector3.Dot(rigidBody.angularVelocity, brakeThrustVector)) != Mathf.Sign(Vector3.Dot(velocityAfterBraking, brakeThrustVector));
+            if (overBraked) brakeThrustVector = default(Vector3);
+            angularThrust += brakeThrustVector;
+        }
+        rigidBody.angularVelocity += angularThrust*rotationalPower*Time.deltaTime;
+
+        if (thrustVector.magnitude < 0.1) {
+            var glide = rigidBody.velocity - RestAtPoint(transform.position);
+            var brakeThrustVector = -glide.normalized * brakingFactor;
+            var velocityAfterBraking = rigidBody.velocity + brakeThrustVector*enginePower*Time.deltaTime;
+            var overBraked = Mathf.Sign(Vector3.Dot(rigidBody.velocity, brakeThrustVector)) != Mathf.Sign(Vector3.Dot(velocityAfterBraking, brakeThrustVector));
+            if (overBraked) brakeThrustVector = default(Vector3);
+            thrustVector += brakeThrustVector;
+        }
+        rigidBody.velocity += thrustVector * Time.deltaTime * enginePower;
+
+        var outputThrustUpDown = Vector3.Dot(thrustVector, transform.up);
+        var outputThrustLeftRight = Vector3.Dot(thrustVector, transform.right);
+        var outputThrustForwardBack = Vector3.Dot(thrustVector, transform.forward);
+
+        var outputAngularThrustUpDown = Vector3.Dot(angularThrust, transform.up);
+        var outputAngularThrustLeftRight = Vector3.Dot(angularThrust, transform.right);
+        var outputAngularThrustForwardBack = Vector3.Dot(angularThrust, transform.forward);
+
+        const float emissionFactor = 50;
+
+        Func<float, float> emissionRateForOutput = output => {
+            if (output < 0.2) return 0;
+            return Mathf.Clamp(output, 0, 1)*emissionFactor;
+        };
+        EngineLeftDownward.emissionRate = emissionRateForOutput(-outputAngularThrustForwardBack + outputThrustUpDown);
+        EngineLeftUpward.emissionRate = emissionRateForOutput(outputAngularThrustForwardBack - outputThrustUpDown);
+        EngineRightDownward.emissionRate = emissionRateForOutput(outputAngularThrustForwardBack + outputThrustUpDown);
+        EngineRightUpward.emissionRate = emissionRateForOutput(-outputAngularThrustForwardBack - outputThrustUpDown);
+
+        EngineBottomForeward.emissionRate = emissionRateForOutput(outputAngularThrustLeftRight);
+        EngineBottomBackward.emissionRate = emissionRateForOutput(-outputAngularThrustLeftRight);
+
+        EngineLeftLeftward.emissionRate = emissionRateForOutput(outputThrustLeftRight);
+        EngineRightRightward.emissionRate = emissionRateForOutput(-outputThrustLeftRight);
+
+        EngineLeftForeward.emissionRate = emissionRateForOutput(-outputAngularThrustUpDown - outputThrustForwardBack);
+        EngineLeftBackward.emissionRate = emissionRateForOutput(outputAngularThrustUpDown + outputThrustForwardBack);
+        EngineRightForeward.emissionRate = emissionRateForOutput(outputAngularThrustUpDown - outputThrustForwardBack);
+        EngineRightBackward.emissionRate = emissionRateForOutput(-outputAngularThrustUpDown + outputThrustForwardBack);
+    }
     private static Vector3 RestAtPoint(Vector3 position) {
+        return default(Vector3);
         return -position.normalized*25;
     }
     void MakeSpeedStreak() {
