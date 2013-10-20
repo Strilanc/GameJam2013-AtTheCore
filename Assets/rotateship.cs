7c2d3ac4624f6f711718cb54b4c36c0245a5a3e5
Solomon Vromans <svromans@gmail.com>
2013-10-20 18:27:15 -0300

    updatemenu

---
+++ Assets/rotateship.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateship : MonoBehaviour {
+	public float rotateSpeed = 10.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+	}
+}
