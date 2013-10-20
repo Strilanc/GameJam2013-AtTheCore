b769c74cb236faf551a370cd885f5abda1f48685
Solomon Vromans <svromans@gmail.com>
2013-10-20 15:40:04 -0300

    Menu Scene added

---
+++ Assets/MegaControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+public class MegaControl : MonoBehaviour {
+	
+	public Mcilhargey start;
+	public Mcilhargey quit;
+	
+	public Vector3 PositionAtDepth{
+		get{
+			return new Vector3(Input.mousePosition.x,Input.mousePosition.y,Mathf.Infinity);
+		}
+	}
+	
+	void Update () {
+		
+		
+		
+		Ray ray = Camera.main.ScreenPointToRay(PositionAtDepth);
+		
+		RaycastHit hit;
+		if(Physics.Raycast(ray.origin,ray.direction,out hit) && Input.GetMouseButton(0)){
+			Mcilhargey mc = hit.transform.GetComponent<Mcilhargey>();
+			if(mc != null){
+				if(mc.action == "start"){
+					Application.LoadLevel("Scene1");
+				}
+			}
+			
+		}
+		
+		if(Physics.Raycast(ray.origin,ray.direction,out hit)){
+			Mcilhargey mc = hit.transform.GetComponent<Mcilhargey>();
+			if(mc != null){
+				mc.guiTEx.color = Color.blue;
+			}
+			
+		}else{
+			start.guiTEx.color = Color.white;
+				quit.guiTEx.color = Color.white;
+		}
+	}
+}
---
+++ Assets/MenuSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSpawner : MonoBehaviour {
+		public Rigidbody Asteroid;
+		public int min = -200;
+		public int max = 200;
+		int randPosition;
+		public int asteroidCount = 0;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		randPosition = Random.Range(min, max);
+		if (asteroidCount > 10){
+			Instantiate(Asteroid, new Vector3(randPosition, randPosition, 200), Quaternion.identity);
+			asteroidCount += 1;
+		}		
+	}
+}
