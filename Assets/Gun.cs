2124db26885039647f2a8e1222bfdf8909a17a46
paul pettipas <paul.pettipas@gmail.com>
2013-10-20 14:52:15 -0300

    gun

---
+++ Assets/Gun.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+	public GameObject projectile;
+	public void Update(){
+		Instantiate(projectile,transform.position,transform.rotation);
+	}
+}
---
+++ Assets/Projectile.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	
+	void Update () {
+		transform.Translate(transform.forward *1000*Time.deltaTime);
+	}
+}
