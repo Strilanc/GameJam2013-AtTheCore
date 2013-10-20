5a5e35a5b4198e93a6c34113ff2875a4863c5058
Craig Gidney <craig.gidney@gmail.com>
2013-10-20 15:38:52 -0300

    More asteroids

--- Assets/AsteroidSpawner.cs
+++ Assets/AsteroidSpawner.cs
@@ -42,7 +42,7 @@ public class AsteroidSpawner : MonoBehaviour {
             var t = Random.Range(0, ThingToInstantiate.Count);
             var x = (GameObject)Instantiate(ThingToInstantiate[t], pos, Quaternion.identity);
             var r = x.GetComponent<Rigidbody>();
-            r.transform.localScale *= Random.Range(0.5f, 1.5f);
+            x.transform.localScale *= Random.Range(0.5f, 1.5f);
             r.velocity = r.transform.position.normalized*5 + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
             r.angularVelocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
             _asteroids.Add(x);
