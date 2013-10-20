7d0ae1b11cf7fcad907cb28be16f8f5681474ab6
Craig Gidney <craig.gidney@gmail.com>
2013-10-20 17:09:16 -0300

    Spin it

---
+++ Assets/SpinStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    rigidbody.angularVelocity += new Vector3(7, 0, 0);
+	    rigidbody.inertiaTensor *= 500;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
