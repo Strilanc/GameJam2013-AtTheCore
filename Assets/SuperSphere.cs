ec0cf7ab64dff9575b881b21fe7223129beafe18
Craig Gidney <craig.gidney@gmail.com>
2013-10-20 18:26:59 -0300

    Ending

---
+++ Assets/SuperSphere.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperSphere : MonoBehaviour {
+	void Start () {
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision other) {
+        Application.LoadLevel("end");
+    }
+}
